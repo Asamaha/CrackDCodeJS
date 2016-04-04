d7ddb18d7c8b84794fa1aff09b6d6b4f7a880cf3
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-03 18:58:05 -0700

    implementin non duplicate element using XOR for now

---
+++ nonRepeat.js
@@ -0,0 +1,12 @@
+//find the non repeating element in an array
+
+//Using XOR
+//This solution will only work if the repeated numbers are even
+var nonDuplicate = function(array) {
+  var first = this[0];
+
+  for(var i = 0; i < array.length; i++) {
+    first ^= array[i];
+  }
+  return first;
+}
