e2d1424310bfed1272a54fe91e45d617e3428e60
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-26 15:27:37 -0700

    sort stack

--- Chap3.js
+++ Chap3.js
@@ -172,8 +172,51 @@ MyQueue.prototype.dequeue = function() {
   return this.outbox.pop();
 };
 
+/*In the classic problem of the Towers of Hanoi, you have 3 rods and N disks of 
+different sizes which can slide onto any tower. 
+The puzzle starts with disks sorted in ascending order of size from top to bottom 
+(e.g., each disk sits on top of an even larger one). You have the following constraints:
+(A) Only one disk can be moved at a time.
+(B) A disk is slid o  the top of one rod onto the next rod.
+(C) A disk can only be placed on top of a larger disk.
+Write a program to move the disks from the  rst rod to the last using Stacks.
+*/
 
+/*write a program to sort a stack in ascending order. 
+You should not make any assump- tions about how the stack is implemented. 
+The following are the only functions that should be used to write this 
+program: push | pop | peek | isEmpty.*/
+function sortStack(stack) {
+  var temp = [];
+  temp.push(stack.pop());
+  while (!isEmpty(stack)) {
+    var curr = stack.pop(),
+      count = 0;
 
+    while (!isEmpty(temp) && curr < peek(temp)) {
+      stack.push(temp.pop());
+      ++count;
+    }
+    temp.push(curr);
+    for (var i = 0; i < count; ++i) {
+      temp.push(stack.pop());
+    }
+  }
+
+  while (!isEmpty(temp)) {
+    stack.push(temp.pop());
+  }
+
+  return stack;
+}
+
+function peek(stack) {
+  return stack[stack.length - 1];
+}
+
+function isEmpty(stack) {
+  return stack.length === 0;
+}
 
 
 
