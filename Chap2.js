ba12cf631c0c38d5672bc8ca9fc291472fb5a83f
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-26 02:03:28 -0700

    additional space

--- Chap2.js
+++ Chap2.js
@@ -89,6 +89,7 @@ function partition(node, x) {
 }
 
 // You have two numbers represented by a linked list, where each node contains a sin- gle digit. The digits are stored in reverse order, such that the 1’s digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+//needs additional space 0(n)
 function sumListsReverseOrder(list1, list2) {
   var head = { next: null }, // pseudo node
     tail = head,
